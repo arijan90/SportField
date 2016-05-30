685c50d3347602ea9fb01c419f166bf225a24986
Arijan Ljoki <arijan_ljoki@hotmail.com>
2016-05-30 14:07:05 +0200

    spacing

--- SportField/FieldViewController.swift
+++ SportField/FieldViewController.swift
@@ -67,7 +67,6 @@ class FieldViewController: UIViewController, MatchPitchProtocol {
                                 })
                         })
                 })
-                
                 self.view.layoutIfNeeded()
         }
     }
